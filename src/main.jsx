ffb9d929ab70c0d78baf60b522e159c5eb761461
Lars Wassermann <lwassermann@babbel.com>
2015-05-17 17:34:38 +0200

    moved abilities tags to their own file

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 'use strict';
 
-import {someChar} from "./storage";
-import splittermond from "./splittermond";
 import React from 'react';
 import * as R from 'ramda';
+
+import splittermond from './splittermond';
+import Abilities from './abilities.jsx';
+import {someChar} from './storage';
 
 
 const TextInput = React.createClass({
@@ -49,59 +51,6 @@ const Name = React.createClass({
       <label className="char-name">
       Name: <TextInput content={this.props.model.name} handleChange={this.renameTo} />
       </label>
-    );
-  }
-});
-
-const Abilities = React.createClass({
-  changeAbility(ability, delta) {
-    var path = ['abilities', ability];
-    this.props.alterPath(path, (R.path(path, this.props.model) || 0) + delta);
-  },
-
-  render() {
-    const abilities = R.map(ability => {
-      return <Ability
-        {...ability}
-        key={ability.name}
-        handleChange={this.changeAbility}
-        char={this.props.model} />;
-    }, splittermond.abilities);
-    return (
-      <table className='abilities'>
-        <thead><tr><th>Name</th><th>∑</th><th>Punkte</th><th>Att 1</th><th>Att 2</th><th>Mod</th></tr></thead>
-        <tbody>{abilities}</tbody>
-      </table>
-    );
-  }
-});
-
-const Ability = React.createClass({
-  aggregated() {
-    return this.props.char.attributes[this.props.attributes[0]]
-      + this.props.char.attributes[this.props.attributes[1]]
-      + (this.props.char.abilities[this.props.name] || 0)
-  },
-  dec() { this.props.handleChange(this.props.name, -1); },
-  inc() { this.props.handleChange(this.props.name, +1); },
-  render() {
-    return (
-      <tr className='ability'>
-        <td className='ability-name'>{this.props.name}</td>
-        <td className='number ability-aggregated'>{this.aggregated()}</td>
-        <td className='number ability-points'>
-          <button className="btn increment" onClick={this.inc}></button>
-          {this.props.char.abilities[this.props.name] || 0}
-          <button className="btn decrement" onClick={this.dec}></button>
-        </td>
-        <td className='number ability-attributes'>
-          <span className='placeholder shorthand attribute-name'>{this.props.attributes[0]}</span>
-          {this.props.char.attributes[this.props.attributes[0]]}</td>
-        <td className='number ability-attributes'>
-          <span className='placeholder shorthand attribute-name'>{this.props.attributes[1]}</span>
-          {this.props.char.attributes[this.props.attributes[1]]}</td>
-        <td className='number ability-mod'>?</td>
-      </tr>
     );
   }
 });
